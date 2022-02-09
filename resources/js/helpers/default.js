f1f31067f7faab25205cc93c8de32109564be99b
Jason Cooper <563137@bah.com>
2022-02-09 02:57:14 -0500

    Wired up proof of concept treemap with zoom in and zoom out

--- resources/js/helpers/default.js
+++ resources/js/helpers/default.js
@@ -6,5 +6,5 @@ let dataNotation = document.createElement("div");
 dataNotation.innerHTML = `Our flareData dataset has ${ flareData.length } records`;
 
 let target = document.getElementById("legend");
-target.appendChild(dataNotation);
+//target.appendChild(dataNotation);
 console.log(flareData);
